710ad7bb76f18cf2b96643b100399bc4b453fff1
Rob Bock <rbock9@gmail.com>
2021-11-12 18:55:47 -0500

    Backend server complete

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-// DEPENDENCIES
-/// get .env variables
+//////////////////////////////////
+// Dependencies
+/////////////////////////////////
+// get .env variables
 require("dotenv").config()
 // pull PORT from .env, give it a default of 3000 (object destructuring)
 const { PORT = 3000, DATABASE_URL } = process.env
@@ -13,26 +15,97 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const morgan = require("morgan")
 
-// DATABASE CONNECTION
 
-// establish connection
+/////////////////////////////////
+// Database Connection
+////////////////////////////////
+// Establish Connection
 mongoose.connect(DATABASE_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true
 })
 
-//MODELS
+// Connection Events
+mongoose.connection
+.on("open", () => console.log("You are connected to Mongo"))
+.on("close", () => console.log("You are disconnected from Mongo"))
+.on("error", (error) => console.log(error))
 
-// MIDDLEWARE
-app.use(cors()) // prevent cors erros, opens up access for frontend
+
+//////////////////////////////
+// Models
+//////////////////////////////
+// The Bookmarks Schema
+const BookmarksSchema = new mongoose.Schema({
+    name: String,
+    url: String
+}, {timestamps: true})
+
+const Bookmarks = mongoose.model("Bookmarks", BookmarksSchema)
+
+
+/////////////////////////////////
+// Middleware
+//////////////////////////////////
+app.use(cors()) // prevent cors errors, opens up access for frontend
 app.use(morgan("dev")) // loggging
 app.use(express.json()) // parse json bodies
 
-// test route
+
+////////////////////////////////
+// Routes
+////////////////////////////////
+// Test Route
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
+// Index Route
+// get request to /bookmarks, returns all bookmarks as json
+app.get("/bookmarks", async (req, res) => {
+    try {
+      // send all bookmarks
+      res.json(await Bookmarks.find({}));
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+});
 
-// SERVER LISTENER
+// Create Route
+// post request to /bookmarks, uses request body to make new bookmark
+app.post("/bookmarks", async (req, res) => {
+    try {
+      // create a new bookmark
+      res.json(await Bookmarks.create(req.body));
+    } catch (error) {
+      res.status(400).json({ error });
+    }
+});
+
+// Update Route
+// put request /bookmarks/:id, updates bookmarks based on id with request body
+app.put("/bookmarks/:id", async (req, res) => {
+    try {
+        // update a bookmark
+        res.json(await Bookmarks.findByIdAndUpdate(req.params.id, req.body, {new: true}));
+      } catch (error) {
+        res.status(400).json({ error });
+      }
+})
+
+// Destroy Route 
+// delete request to /bookmarks/:id, deletes the bookmark specified
+app.delete("/bookmarks/:id", async (req, res) => {
+    try {
+        // delete a bookmark
+        res.json(await Bookmarks.findByIdAndRemove(req.params.id));
+      } catch (error) {
+        res.status(400).json({ error });
+      }
+})
+
+
+/////////////////////////////////
+// Server Listener
+/////////////////////////////////
 app.listen(PORT, () => {console.log(`listening on PORT ${PORT}`)})
